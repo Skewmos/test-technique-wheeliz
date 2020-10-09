16f52387457b240eb8f5579173ead280702ed42f
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-10-09 15:44:54 +0200

    feat: Adding apollo connection

--- client2/src/index.js
+++ client2/src/index.js
@@ -6,12 +6,24 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // You will have to configure ApolloClient To get it working
-// The url of the schema is => localhost:4000/graphql
-// The import remains here to give you some hints on what to use
+
+const link  = createHttpLink({ uri: "http://localhost:4000/graphql" })
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
+
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
+
+// The url of the schema is => localhost:4000/graphql
+// The import remains here to give you some hints on what to use
