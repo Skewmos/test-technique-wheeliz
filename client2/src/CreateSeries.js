1b3d07950b5ae8085495ba3d53433427b0f39bd9
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-10-10 10:09:33 +0200

    feat: CreateSerie system

--- client2/src/CreateSeries.js
+++ client2/src/CreateSeries.js
@@ -1,15 +1,23 @@
 import React from 'react';
+
 import { Mutation } from 'react-apollo';
 import { CREATE_SERIE } from './Mutations/Series';
 import { SERIES_QUERY } from './Query/Series'
+import { Button } from 'reactstrap';
+
 
 export default function CreateSeries() {
   const title = "new serie";
   const tag = "asset";
   const banner_url = "https://via.placeholder.com/600/FF0000";
+
   return (
-    <div>
-        Just Make a button that will send a mutation to add a new serie
+    <div className="container">
+    <Mutation mutation={CREATE_SERIE} variables={{title, tag, banner_url}}>
+      {createSeries => (
+        <Button  className="btn-block mt-5 mb-5" color="primary" onClick={createSeries}>NEW </Button>
+      )}
+    </Mutation>
     </div>
-  )
+  );
 }
