26da8d4956e6c6bd3f2f956e0ab706c603d7dba5
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-10-10 10:06:51 +0200

    feat: Creating mutation for creat new serie

--- client2/src/Mutations/Series.js
+++ client2/src/Mutations/Series.js
@@ -1,8 +1,16 @@
 import gql from 'graphql-tag';
+import { tagPropType } from 'reactstrap/lib/utils';
+import CreateSeries from '../CreateSeries';
 
 // Mutation to create a serie
 const CREATE_SERIE = gql`
-
+  mutation CreateSeries($title: String!, $tag: String!, $banner_url: String!) {
+    createSeries(title: $title, tag: $tag, banner_url: $banner_url) {
+      title
+      tag
+      banner_url
+    }
+  }
 `;
 
 export {
