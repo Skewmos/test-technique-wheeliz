ee40d8a2e1d9de617bb2e4fa441e20b5c43d456b
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-10-10 10:08:30 +0200

    feat: Creating carousel to recep all  of serie

--- client2/src/components/carousel/carousel.js
+++ client2/src/components/carousel/carousel.js
@@ -1,7 +1,36 @@
 import React, {Component} from 'react';
+import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption} from 'reactstrap';
+import {SERIES_QUERY} from '../../Query/Series';
+import {Query} from 'react-apollo';
+import { gql, useQuery } from '@apollo/client';
+import { UncontrolledCarousel } from 'reactstrap';
+import { useState } from 'react';
+
 
 class MediaCard extends Component {
-    // Here it will be the Carousel component
+    
+    constructor(props) {
+        super(props);
+        const {serie} = this.props;
+    }
+
+
+render() {
+  const {serie} = this.props;
+  return (
+    <>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div className="carousel-inner"></div>
+          <div className="carousel-item active">
+            <img className="d-block w-100" src={serie.banner_url} alt="First slide"/>
+            <div className="carousel-caption d-none d-md-block">
+            </div>
+        </div>  
+      </div>
+    </>
+  );
 }
+}
+
 
 export default MediaCard;
